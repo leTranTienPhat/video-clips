d6b165c6a6870a3b5129fdf1774feefb15a8328d
Lê Trần Tiến Phát <tienphat0707@gmail.com>
2023-05-09 08:12:45 +0700

    Added shared modal

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+  // providers: [ModalService]
+})
+export class ModalComponent {
+  @Input() modalID = ''
+
+  constructor(public modal: ModalService) {
+  }
+
+  closeModal() {
+    this.modal.toggleModal(this.modalID)
+  }
+}
